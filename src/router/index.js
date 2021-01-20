358c396d7374356a1e31336682742250addbeac7
Alejandro Millan <darkslave@live.com>
2021-01-20 17:07:22 -0500

    auth layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,46 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+import Auth from "@/layouts/Auth";
+
+import AuthView from "@/views/AuthView";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Auth",
+    component: Auth,
+    children: [
+      {
+        path: "",
+        name: "AuthView",
+        component: AuthView,
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
