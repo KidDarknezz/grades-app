93eebe6cb686440124985489f4de404f31240948
Alejandro Millan <darkslave@live.com>
2021-01-27 10:18:15 -0500

    icons and name

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,30 @@
 module.exports = {
   pluginOptions: {
     quasar: {
-      importStrategy: 'kebab',
-      rtlSupport: false
-    }
+      importStrategy: "kebab",
+      rtlSupport: false,
+    },
   },
-  transpileDependencies: [
-    'quasar'
-  ]
-}
+  transpileDependencies: ["quasar"],
+  pwa: {
+    name: "My Grades",
+    short_name: "My Grades",
+    themeColor: "#000",
+    msTileColor: "#000000",
+    backgroundColor: "#ffffff",
+    manifestOptions: {
+      icons: [
+        {
+          src: "@/assets/icon-512.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "@/assets/icon-192.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+  },
+};
