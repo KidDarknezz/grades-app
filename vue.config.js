4980b80c537c4c07c8a31e639d1681d5067bd004
Alejandro Millan <darkslave@live.com>
2021-01-29 12:40:56 -0500

    update path for service worker

--- vue.config.js
+++ vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: "service-worker.js",
+      swSrc: "src/registerServiceWorker.js",
       // ...other Workbox options...
       exclude: [/_redirects/],
     },
