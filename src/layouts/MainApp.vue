7423a076df26c69205188354543aea226eceb808
unknown <darkslave@live.com>
2021-01-30 13:50:31 -0500

    prompt install

--- src/layouts/MainApp.vue
+++ src/layouts/MainApp.vue
@@ -18,6 +18,7 @@
           no-caps
           to="/profile"
           v-if="$route.fullPath.includes('profile') && mode == 'browser tab'"
+          @click="deferredPrompt.prompt()"
         />
         <q-btn
           flat
@@ -239,6 +240,7 @@ export default {
     return {
       installBanner: this.returnIfShowInstallPrompt(),
       mode: "",
+      deferredPrompt: "",
       drawer: false,
       createNewAssignature: false,
       randomColorIndex: this.returnRandomNumber(),
@@ -302,6 +304,29 @@ export default {
   mounted() {
     this.returnDisplayMode();
     this.getUserInfoAndAssignatures(localStorage.getItem("mgAppUid"));
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+
+      // addBtn.addEventListener('click', (e) => {
+      //   // hide our user interface that shows our A2HS button
+      //   addBtn.style.display = 'none';
+      //   // Show the prompt
+      //   deferredPrompt.prompt();
+      //   // Wait for the user to respond to the prompt
+      //   deferredPrompt.userChoice.then((choiceResult) => {
+      //       if (choiceResult.outcome === 'accepted') {
+      //         console.log('User accepted the A2HS prompt');
+      //       } else {
+      //         console.log('User dismissed the A2HS prompt');
+      //       }
+      //       deferredPrompt = null;
+      //     });
+      // });
+    });
   },
 };
 </script>
