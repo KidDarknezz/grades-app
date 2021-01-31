a7ef4c1b1952694088bce3ea73465f11b78c31ce
unknown <darkslave@live.com>
2021-01-30 20:38:53 -0500

    test

--- src/layouts/MainApp.vue
+++ src/layouts/MainApp.vue
@@ -3,9 +3,8 @@
     <q-header class="bg-grey-2 text-black">
       <q-toolbar v-if="!$route.fullPath.includes('selected-assignature')">
         <q-toolbar-title>
-          <!-- <q-btn dense flat round icon="menu" @click="drawer = !drawer" /> -->
           <div class="text-h6 w700">
-            <span :class="`text-${userData.profileColor}`">my</span>Grades
+            <span :class="`text-${userData.profileColor}`">my</span>Grades-
           </div>
         </q-toolbar-title>
         <q-btn
@@ -29,89 +28,6 @@
         />
       </q-toolbar>
     </q-header>
-    <q-drawer v-model="drawer" side="left" behavior="mobile" elevated>
-      <q-scroll-area
-        style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd"
-      >
-        <q-list padding>
-          <q-item
-            clickable
-            v-ripple
-            :active="$route.fullPath.includes('my-assignatures')"
-            to="/my-assignatures"
-          >
-            <q-item-section avatar>
-              <q-icon name="list_alt" />
-            </q-item-section>
-
-            <q-item-section>
-              My assignatures
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            :active="$route.fullPath.includes('closed-assignatures')"
-            to="/closed-assignatures"
-          >
-            <q-item-section avatar>
-              <q-icon name="inventory_2" />
-            </q-item-section>
-
-            <q-item-section>
-              Closed assignatures
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            :active="$route.fullPath.includes('profile')"
-            to="/profile"
-          >
-            <q-item-section avatar>
-              <q-icon name="person" />
-            </q-item-section>
-
-            <q-item-section>
-              My account
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple @click="logoutCurrentUser()">
-            <q-item-section avatar>
-              <q-icon name="login" />
-            </q-item-section>
-
-            <q-item-section>
-              Logout
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-scroll-area>
-
-      <q-img
-        class="absolute-top"
-        src="https://cdn.quasar.dev/img/material.png"
-        style="height: 150px"
-      >
-        <div class="absolute-bottom bg-transparent">
-          <q-avatar
-            size="60px"
-            class="q-mb-sm bg-white"
-            v-if="userData.profileAvatar"
-          >
-            <img
-              :src="require(`@/assets/avatars/${userData.profileAvatar}.png`)"
-            />
-          </q-avatar>
-          <div class="text-weight-bold">
-            {{ userData.name }} {{ userData.lastName }}
-          </div>
-        </div>
-      </q-img>
-    </q-drawer>
     <q-page-container class="bg-grey-2">
       <transition name="fade">
         <router-view :new="createNewAssignature" />
