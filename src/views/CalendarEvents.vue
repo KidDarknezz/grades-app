e6f0914c33b3ac807900433b9deb7a20ac472b5d
Alejandro Millan <darkslave@live.com>
2021-02-02 16:09:17 -0500

    calendar design

--- src/views/CalendarEvents.vue
+++ src/views/CalendarEvents.vue
@@ -3,5 +3,36 @@
     <div class="row q-mt-lg q-mb-lg">
       <div class="text-subtitle2 text-grey-7">Upcoming events</div>
     </div>
+    <div class="col">
+      <q-card flat class="assignature-card q-mb-md">
+        <q-card-section>
+          February 14, 2020
+        </q-card-section>
+        <q-card-section>
+          <q-list separator>
+            <q-item>
+              <q-item-section avatar>
+                <div
+                  class="bg-purple full-width"
+                  style="padding: 4px;border-radius: 4px;"
+                ></div>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label class="text-black"
+                  ><div class="text-bold q-mb-sm">Parcial 1 Matematicas</div>
+                  <div>14:30 P.M.</div></q-item-label
+                >
+              </q-item-section>
+              <q-item-section avatar>
+                <q-btn-group flat rounded>
+                  <q-btn dense icon="close" class="text-red" size="sm" />
+                  <q-btn dense icon="edit" class="text-warning" size="sm" />
+                </q-btn-group>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
